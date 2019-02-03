634da96485dfc6bde6231db0da2c0cc1e7983252
korolrom <roman@caltech.edu>
2019-02-03 00:02:42 -0800

    Finished with part 2. Should create a branch for Morse potential

--- P3_BasicMD/md.py
+++ P3_BasicMD/md.py
@@ -6,7 +6,7 @@ import SHO as sho
 #input parameters:
 m=1.
 omega=1.
-dt=1e-2
+dt=1e-1
 t=10.
 
 #future sampler
@@ -33,10 +33,17 @@ PQ=np.array([AnalPQ, num.Euler(p0,q0,t,dt,m,da),num.Verlet(p0,q0,t,dt,m,da),
 
 #get the energies on the trajectory
 E=np.moveaxis(np.array([sho.get_ke(PQ[:,0,:],m),sho.get_pe(PQ[:,1,:],omega,m)]),1,0)
-print(np.shape(E))
-print(np.shape(PQ))
+#print(np.shape(E))
+#print(np.shape(PQ))
+
+#Compute global energy conservation and the standard deviation in E
+Ebar=np.cumsum(np.sum(E,axis=1),axis=-1)/(np.arange(int(t/dt))+1)
+print(np.shape(Ebar))
+print(np.shape((np.sum(E,axis=1)-Ebar)**2))
+Esigma=np.cumsum((np.sum(E,axis=1)-Ebar)**2,axis=-1)/(np.arange(int(t/dt))+1)
+
+# Plots of p and q with time, energies with time
 n=np.size(E,axis=0)
-# Plot of p and q with time
 Style=[['r-','r--','r:','r-.'],['g-','g--','g:','g-.'],['b-','b--','b:','b-.'],['k-','k--','k:','k-.'],['m-','m--','m:','m-.'],['c-','c--','c:','c-.']]
 fig = plt.figure()
 subj=1
@@ -47,10 +54,11 @@ for item in [PQ,E]:
         plt.plot(np.arange(0,t,dt),item[i,1,:],Style[i][1])
         if subj==2:
             plt.plot(np.arange(0,t,dt),np.sum(item[i,:,:],axis=0),Style[i][2])
+            plt.plot(np.arange(0,t,dt),Ebar[i,:],Style[i][3])
+            plt.plot(np.arange(0,t,dt),np.sqrt(Esigma[i,:]),Style[i][3])
     subj=subj+1
 plt.show()
 #print(p0,q0)
-
 
 
 # x=sp.symbols('x m omega')
